cffeae716242be5299f0193348cc3be9cd96c192
Philipp <PhilippHentschel@t-online.de>
2016-07-14 20:33:19 +0200

    fixed spotify endless update bug

--- Snip/Players/Spotify.cs
+++ Snip/Players/Spotify.cs
@@ -66,7 +66,7 @@ namespace Winter
                                 {
                                     this.SaveBlankImage();
                                 }
-
+                                this.LastTitle = spotifyTitle;
                                 TextHandler.UpdateTextAndEmptyFilesMaybe(Globals.ResourceManager.GetString("NoTrackPlaying"));
                             }
                             else
